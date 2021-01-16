7ab9bf1bd28f3d7e80d6783ca7512276001a942b
goranaviani <goranaviani@gmail.com>
2021-01-16 12:40:12 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main_menu():
+    print('************************************************')
+    print('Hello and welcome to the End to end word game!')
+    print('************************************************')
+
+if __name__ == '__main__':
+    main_menu()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
