9fd45737691433341b4312d2392a3068c42261ea
goranaviani <goranaviani@gmail.com>
2021-01-16 21:44:51 +0100

    fixup on the checks

--- check_word_letter.py
+++ check_word_letter.py
@@ -1,15 +1,12 @@
 
 
-def word_checker(list_of_words, user_input, first_letter_for_the_next_word):
-    list_of_words.append(user_input)
+def word_checker(list_of_words, user_input, last_letter):
 
     if len(list_of_words) > 1:
-        print(first_letter_for_the_next_word)
-
-        if user_input[0] != first_letter_for_the_next_word:
+        if user_input[0] != last_letter:
             print('whoops, you made a mistake!')
             return 'mistake', list_of_words, ''
 
 
-    return 'ok', list_of_words, first_letter_for_the_next_word
+    return 'ok', list_of_words, last_letter
 
