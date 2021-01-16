2d6df901e7c0f361c2f976c0096274b203768138
goranaviani <goranaviani@gmail.com>
2021-01-16 12:48:05 +0100

    simple user input done

--- user_input.py
+++ user_input.py
@@ -1,6 +1,12 @@
 def get_user_input():
     user_input = ''
+    list_of_words = []
     while user_input != 'x':
         user_input = input('Write here: ')
+        user_input = user_input.lower()
+        list_of_words.append(user_input)
+        
+
+    print(list_of_words)
 
     exit()
