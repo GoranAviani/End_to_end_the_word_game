c0f8bd207532a363139312bb05ff014042776d94
goranaviani <goranaviani@gmail.com>
2021-01-20 16:38:49 +0100

    cleaning the user input fun

--- check_word_letter.py
+++ check_word_letter.py
@@ -4,13 +4,10 @@ def word_checker(list_of_words, user_input):
     list_of_words.append(user_input)
     if len(list_of_words) > 1:
         last_letter = list_of_words[-2][len(list_of_words[-2]) - 1: len(list_of_words[-2])]
-    else:
-        last_letter = ''
-
-    if len(list_of_words) > 1:
         if user_input[0] != last_letter:
             return 'mistake', list_of_words
-
+    else:
+        last_letter = ''
 
     return 'ok', list_of_words
 
